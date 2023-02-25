d32319d5c074a01ec4140d9ced0b6ce1e356b1cc
masaru-yamaguchi-wa <masaru.yamaguchi@woven-planet.global>
2023-02-26 08:46:25 +0900

    add test function for git command component

--- src/commandData.js
+++ src/commandData.js
@@ -5,70 +5,81 @@ export const command = [
         "target": ".",
         "option": "",
         "detail": "work treeからファイルをステージングすることが可能",
-        "detail2": "git add <targetFile>"
+        "detail2": "git add <targetFile>",
+        "func": (position) => { console.log(position) }
     },
     {
         "command": "git commit -m",
         "target": "message",
         "option": "",
         "detail": "stageからlocal repoにあげることが可能。messageは必須",
-        "detail2": "git commit -m <message>"
+        "detail2": "git commit -m <message>",
+        "func": (position) => { console.log(position) }
+
     },
     {
         "command": "git push",
         "target": "origin",
         "option": "main",
         "detail": "local repo から remote repo にあげることが可能",
-        "detail2": "git push <targetBranch> <sender branch>"
+        "detail2": "git push <targetBranch> <sender branch>",
+        "func": (position) => { console.log(position) }
     },
     {
         "command": "git pull",
         "target": "origin",
         "option": "main",
         "detail": "remote repo から local repo に引き抜くことが可能",
-        "detail2": "git pull <targetBranch> <senderBranch>"
+        "detail2": "git pull <targetBranch> <senderBranch>",
+        "func": (position) => { console.log(position) }
     },
     {
         "command": "git status",
         "target": "",
         "option": "",
         "detail": "work treeの状態を確認することが可能",
-        "detail2": "git status"
+        "detail2": "git status",
+        "func": (position) => { console.log(position) }
     },
     {
         "command": "git branch",
         "target": "",
         "option": "",
         "detail": "branchの確認や生成が可能",
-        "detail2": "git branch 確認 git branch <newBranch> 生成"
+        "detail2": "git branch 確認 git branch <newBranch> 生成",
+        "func": (position) => { console.log(position) }
     },
     {
         "command": "git checkout",
         "target": "",
         "option": "",
         "detail": "branchの生成や移動が可能。移動する場合こちらを使う",
-        "detail2": "git checkout -b <branchName> 生成と移動"
+        "detail2": "git checkout -b <branchName> 生成と移動",
+        "func": (position) => { console.log(position) }
     },
     {
         "command": "git reset",
         "target": "commit",
         "option": "",
         "detail": "pushの取り消しが可能",
-        "detail2": "git reset --head <commitID>"
+        "detail2": "git reset --head <commitID>",
+        "func": (position) => { console.log(position) }
     },
     {
         "command": "git revert",
         "target": "commit",
         "option": "",
         "detail": "commitの取り消しが可能",
-        "detail2": "git revert <commitID>"
+        "detail2": "git revert <commitID>",
+        "func": (position) => { console.log(position) }
     },
     {
         "command": "git merge",
         "target": "branch",
         "option": "branch",
         "detail": "branch同士をマージする",
-        "detail2": "git merge <baseBranch> <senderBranch>"
+        "detail2": "git merge <baseBranch> <senderBranch>",
+        "func": (position) => { console.log(position) }
     },
 ]
 
--- src/component/GitCommand.jsx
+++ src/component/GitCommand.jsx
@@ -1,19 +1,58 @@
+import { useState } from "react"
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { command } from "../commandData"
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
 
 export const GitCommand = () => {
+    const [open, setOpen] = useState(false);
+    const [commandIndex, setCommandIndex] = useState(0);
+    const handleOpen = (e) => {
+        setCommandIndex(e.currentTarget.id)
+        setOpen(true)
+    };
+    const handleClose = (e) => {
+        setOpen(false)
+        command[commandIndex].func("on close function")
+    };
     return (
         <>
             <Box height={"97%"} width={"94%"} border={2} borderRadius={2} sx={{ borderColor: "#d9d9d9", color: "#d9d9d9", overflow: "auto" }}>
                 {command.map((e, i) => {
                     return (
-                        <Box key={i} width={"97%"} border={2} borderRadius={2} sx={{ borderColor: "#d9d9d9", color: "#d9d9d9", margin: "3% auto" }}>
-                            <Typography align="left" sx={{ margin: "1% 2%" }} variant="h5" >{e.command}</Typography>
-                            <hr style={{ marginTop: 0 }} />
-                            <Typography align="left" sx={{ margin: "1% 2%" }} variant="body1">{e.detail}</Typography>
-                            <Typography align="left" sx={{ margin: "1% 2%" }} variant="body2">{e.detail2}</Typography>
-                        </Box>
+                        <div key={i} >
+                            <Box id={i} width={"97%"} border={2} borderRadius={2} onClick={handleOpen} sx={{ borderColor: "#d9d9d9", color: "#d9d9d9", margin: "3% auto" }}>
+                                <Typography align="left" sx={{ margin: "1% 2%" }} variant="h5" >{e.command}</Typography>
+                                <hr style={{ marginTop: 0 }} />
+                                <Typography align="left" sx={{ margin: "1% 2%" }} variant="body1">{e.detail}</Typography>
+                                <Typography align="left" sx={{ margin: "1% 2%" }} variant="body2">{e.detail2}</Typography>
+                                <Typography align="left" sx={{ margin: "1% 2%" }} variant="body2">{e.target}</Typography>
+                            </Box>
+                            <Modal
+                                aria-labelledby="modal-modal-title"
+                                aria-describedby="modal-modal-description"
+                                open={open}
+                                onClose={handleClose}
+                                slotProps={{
+                                    backdrop: {
+                                        invisible: false,
+                                        sx: { backgroundColor: "#00000020" }
+                                    },
+                                }}
+                            >
+                                <Box sx={{
+                                    position: 'absolute', top: '50%', left: '50%', backgroundColor: "background.paper", transform: 'translate(-50%, -50%)', boxShadow: 24, p: 4, borderRadius: "10px"
+                                }}
+                                >
+                                    <h2 id="child-modal-title">対象ファイル/リポジトリ/ブランチの選択</h2>
+                                    <p id="child-modal-description">
+                                        Lorem ipsum, dolor sit amet consectetur adipisicing elit.{i}
+                                    </p>
+                                    <Button variant="outlined" sx={{ textTransform: "none" }} onClick={handleClose} >{command[commandIndex].target === "" ? "Just do it's" : `target:${command[commandIndex].target}`} </Button>
+                                </Box>
+                            </Modal>
+                        </div>
                     )
 
                 })}
